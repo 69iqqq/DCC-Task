0f7e8ff508f91ac6c8a07e484d06167dd8b3e718
69iqqq <abirdasgupta09@gmail.com>
2024-07-06 03:12:10 +0530

    touch phone support and UI changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,13 @@ function Header() {
     <div
       style={{
         backgroundColor: "#665c54",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
+        // display: "flex",
+        // alignItems: "center",
+        // justifyContent: "space-between",
       }}
     >
       {/* <img src='src/components/logo.png ' style={{ width: "50px" }} /> */}
-      <header
-        style={{
-          fontFamily: "poppins",
-          fontSize: "30px",
-          color: "#fbf1c7",
-          fontWeight: "bold",
-        }}
-      >
-       MEMEEEEEEEEAS
-      </header>
+      <header className='header'>MEMEEEEEEEEAS</header>
       {/* <img src='src/components/react.svg ' style={{ width: "50px" }} /> */}
     </div>
   );
