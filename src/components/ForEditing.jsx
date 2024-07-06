21a808c59893801ee983ae989584b6ccf02003b1
69iqqq <abirdasgupta09@gmail.com>
2024-07-07 00:59:24 +0530

    added a bit support for mobile phone

--- src/components/ForEditing.jsx
+++ src/components/ForEditing.jsx
@@ -61,8 +61,175 @@
 // }
 
 // export default ForEditing;
+// import React, { useState, useEffect, useRef, useCallback } from "react";
+// import Draggable from "react-draggable";
+// import "../../src/App.css";
+
+// function ForEditing({ style }) {
+//   const [input, setInput] = useState(false);
+//   const [text, setText] = useState("Click on Me to Edit");
+//   const [textColor, setTextColor] = useState("#000000");
+//   const [fontSize, setFontSize] = useState(18);
+//   const [rotation, setRotation] = useState(0); 
+//   const [sliderDragging, setSliderDragging] = useState(false); 
+
+//   const inputRef = useRef(null);
+//   const colorPickerRef = useRef(null);
+//   const fontSizeSliderRef = useRef(null);
+//   const rotationSliderRef = useRef(null);
+
+//   const enterEditMode = () => {
+//     setInput(true);
+//   };
+
+//   const exitEditMode = () => {
+//     setInput(false);
+//     setText(text.replace(/\n/g, "<br>"));
+//   };
+
+//   const handleKeyDown = useCallback((e) => {
+//     if (e.ctrlKey && e.keyCode === 40) {
+//       colorPickerRef.current.click();
+//     } else if (e.ctrlKey && e.keyCode === 38) {
+//       fontSizeSliderRef.current.focus();
+//     } else if (e.key === "Enter" && e.shiftKey) {
+//       const { selectionStart, selectionEnd, value } = inputRef.current;
+//       const newValue =
+//         value.substring(0, selectionStart) +
+//         "\n" +
+//         value.substring(selectionEnd);
+
+//       setText(newValue);
+//       e.preventDefault(); 
+//     }
+//   }, []);
+
+//   const handleSingleClick = () => {
+//     setInput(true);
+//   };
+
+//   const handleTouchStart = () => {
+//     setInput(true);
+//   };
+
+//   const handleColorChange = (color) => {
+//     setTextColor(color);
+//   };
+
+//   const handleFontSizeChange = (value) => {
+//     setFontSize(value);
+//   };
+
+//   const handleRotationChange = (value) => {
+//     setRotation(value);
+//   };
+
+//   const handleSliderDragStart = () => {
+//     setSliderDragging(true);
+//   };
+
+//   const handleSliderDragStop = () => {
+//     setSliderDragging(false);
+//   };
+
+//   useEffect(() => {
+//     if (input && inputRef.current) {
+//       inputRef.current.focus();
+//       inputRef.current.select();
+//     }
+//   }, [input]);
+
+//   return (
+//     <Draggable disabled={sliderDragging}>
+//       <div
+//         style={{ cursor: "pointer" }}
+//         onClick={handleSingleClick}
+//         onTouchStart={handleTouchStart}
+//       >
+//         {input ? (
+//           <div>
+//             <textarea
+//               ref={inputRef}
+//               value={text}
+//               onChange={(e) => setText(e.target.value)}
+//               onBlur={exitEditMode}
+//               onKeyDown={handleKeyDown}
+//               autoFocus
+//               onClick={(e) => e.target.select()}
+//               onTouchStart={(e) => e.target.select()}
+//               style={{
+//                 border: "none",
+//                 outline: "none",
+//                 background: "transparent",
+//                 fontSize: `${fontSize}px`,
+//                 fontWeight: "bold",
+//                 color: textColor,
+//                 width: "300px", 
+//                 resize: "none", 
+//                 transform: `rotate(${rotation}deg)`, 
+//               }}
+//             />
+//             <input
+//               ref={colorPickerRef}
+//               type='color'
+//               value={textColor}
+//               onChange={(e) => handleColorChange(e.target.value)}
+//               onClick={(e) => e.stopPropagation()}
+//               onTouchStart={(e) => e.stopPropagation()}
+//             />
+//             <input
+//               ref={fontSizeSliderRef}
+//               type='range'
+//               min='12'
+//               max='36'
+//               step='2'
+//               value={fontSize}
+//               onChange={(e) => handleFontSizeChange(parseInt(e.target.value))}
+//               onClick={(e) => e.stopPropagation()}
+//               onTouchStart={(e) => e.stopPropagation()}
+//               style={{ marginLeft: "10px" }}
+//             />
+//             <input
+//               ref={rotationSliderRef}
+//               type='range'
+//               min='-180'
+//               max='180'
+//               step='1'
+//               value={rotation}
+//               onChange={(e) => handleRotationChange(parseInt(e.target.value))}
+//               onClick={(e) => e.stopPropagation()}
+//               onTouchStart={(e) => e.stopPropagation()}
+//               onTouchStart={handleSliderDragStart}
+//               onTouchEnd={handleSliderDragStop}
+//               onMouseDown={handleSliderDragStart}
+//               onMouseUp={handleSliderDragStop}
+//               onMouseLeave={handleSliderDragStop}
+//               style={{ marginLeft: "10px" }}
+//             />
+//           </div>
+//         ) : (
+//           <h1
+//             style={{
+//               color: textColor,
+//               fontSize: `${fontSize}px`,
+//               fontFamily: "Poppins",
+//               fontWeight: "600",
+//               transform: `rotate(${rotation}deg)`, 
+//             }}
+//             onClick={enterEditMode}
+//             onTouchStart={handleTouchStart}
+//             dangerouslySetInnerHTML={{ __html: text }} 
+//           />
+//         )}
+//       </div>
+//     </Draggable>
+//   );
+// }
+
+// export default ForEditing;
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import Draggable from "react-draggable";
+import { ChromePicker } from "react-color";
 import "../../src/App.css";
 
 function ForEditing({ style }) {
@@ -70,13 +237,9 @@ function ForEditing({ style }) {
   const [text, setText] = useState("Click on Me to Edit");
   const [textColor, setTextColor] = useState("#000000");
   const [fontSize, setFontSize] = useState(18);
-  const [rotation, setRotation] = useState(0); 
-  const [sliderDragging, setSliderDragging] = useState(false); 
+  const [rotation, setRotation] = useState(0);
 
   const inputRef = useRef(null);
-  const colorPickerRef = useRef(null);
-  const fontSizeSliderRef = useRef(null);
-  const rotationSliderRef = useRef(null);
 
   const enterEditMode = () => {
     setInput(true);
@@ -88,10 +251,8 @@ function ForEditing({ style }) {
   };
 
   const handleKeyDown = useCallback((e) => {
-    if (e.ctrlKey && e.keyCode === 40) {
-      colorPickerRef.current.click();
-    } else if (e.ctrlKey && e.keyCode === 38) {
-      fontSizeSliderRef.current.focus();
+    if (e.ctrlKey && e.keyCode === 38) {
+      inputRef.current.focus();
     } else if (e.key === "Enter" && e.shiftKey) {
       const { selectionStart, selectionEnd, value } = inputRef.current;
       const newValue =
@@ -100,7 +261,7 @@ function ForEditing({ style }) {
         value.substring(selectionEnd);
 
       setText(newValue);
-      e.preventDefault(); 
+      e.preventDefault();
     }
   }, []);
 
@@ -113,7 +274,7 @@ function ForEditing({ style }) {
   };
 
   const handleColorChange = (color) => {
-    setTextColor(color);
+    setTextColor(color.hex);
   };
 
   const handleFontSizeChange = (value) => {
@@ -124,14 +285,6 @@ function ForEditing({ style }) {
     setRotation(value);
   };
 
-  const handleSliderDragStart = () => {
-    setSliderDragging(true);
-  };
-
-  const handleSliderDragStop = () => {
-    setSliderDragging(false);
-  };
-
   useEffect(() => {
     if (input && inputRef.current) {
       inputRef.current.focus();
@@ -139,90 +292,88 @@ function ForEditing({ style }) {
     }
   }, [input]);
 
+  const stopPropagation = (e) => {
+    e.stopPropagation();
+  };
+
   return (
-    <Draggable disabled={sliderDragging}>
-      <div
-        style={{ cursor: "pointer" }}
-        onClick={handleSingleClick}
-        onTouchStart={handleTouchStart}
-      >
-        {input ? (
-          <div>
-            <textarea
-              ref={inputRef}
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-              onBlur={exitEditMode}
-              onKeyDown={handleKeyDown}
-              autoFocus
-              onClick={(e) => e.target.select()}
-              onTouchStart={(e) => e.target.select()}
+    <div>
+      <Draggable>
+        <div
+          style={{ cursor: "pointer" }}
+          onClick={handleSingleClick}
+          onTouchStart={handleTouchStart}
+        >
+          {input ? (
+            <div>
+              <textarea
+                ref={inputRef}
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                onBlur={exitEditMode}
+                onKeyDown={handleKeyDown}
+                autoFocus
+                onClick={(e) => e.target.select()}
+                onTouchStart={(e) => e.target.select()}
+                style={{
+                  border: "none",
+                  outline: "none",
+                  background: "transparent",
+                  fontSize: `${fontSize}px`,
+                  fontWeight: "bold",
+                  color: textColor,
+                  width: "300px",
+                  resize: "none",
+                  transform: `rotate(${rotation}deg)`,
+                }}
+              />
+            </div>
+          ) : (
+            <h1
               style={{
-                border: "none",
-                outline: "none",
-                background: "transparent",
-                fontSize: `${fontSize}px`,
-                fontWeight: "bold",
                 color: textColor,
-                width: "300px", 
-                resize: "none", 
-                transform: `rotate(${rotation}deg)`, 
+                fontSize: `${fontSize}px`,
+                fontFamily: "Poppins",
+                fontWeight: "600",
+                transform: `rotate(${rotation}deg)`,
               }}
+              onClick={enterEditMode}
+              onTouchStart={handleTouchStart}
+              dangerouslySetInnerHTML={{ __html: text }}
             />
-            <input
-              ref={colorPickerRef}
-              type='color'
-              value={textColor}
-              onChange={(e) => handleColorChange(e.target.value)}
-              onClick={(e) => e.stopPropagation()}
-              onTouchStart={(e) => e.stopPropagation()}
-            />
-            <input
-              ref={fontSizeSliderRef}
-              type='range'
-              min='12'
-              max='36'
-              step='2'
-              value={fontSize}
-              onChange={(e) => handleFontSizeChange(parseInt(e.target.value))}
-              onClick={(e) => e.stopPropagation()}
-              onTouchStart={(e) => e.stopPropagation()}
-              style={{ marginLeft: "10px" }}
-            />
-            <input
-              ref={rotationSliderRef}
-              type='range'
-              min='-180'
-              max='180'
-              step='1'
-              value={rotation}
-              onChange={(e) => handleRotationChange(parseInt(e.target.value))}
-              onClick={(e) => e.stopPropagation()}
-              onTouchStart={(e) => e.stopPropagation()}
-              onTouchStart={handleSliderDragStart}
-              onTouchEnd={handleSliderDragStop}
-              onMouseDown={handleSliderDragStart}
-              onMouseUp={handleSliderDragStop}
-              onMouseLeave={handleSliderDragStop}
-              style={{ marginLeft: "10px" }}
-            />
-          </div>
-        ) : (
-          <h1
-            style={{
-              color: textColor,
-              fontSize: `${fontSize}px`,
-              fontFamily: "Poppins",
-              fontWeight: "600",
-              transform: `rotate(${rotation}deg)`, 
-            }}
-            onClick={enterEditMode}
-            onTouchStart={handleTouchStart}
-            dangerouslySetInnerHTML={{ __html: text }} 
-          />
-        )}
+          )}
+        </div>
+      </Draggable>
+      <div
+        style={{ position: "fixed", bottom: "10px", right: "10px" }}
+        onTouchStart={stopPropagation}
+      >
+        <ChromePicker color={textColor} onChange={handleColorChange} />
       </div>
-    </Draggable>
+      <div
+        style={{ position: "fixed", bottom: "50px", right: "10px" }}
+        onTouchStart={stopPropagation}
+      >
+        <input
+          type='range'
+          min='12'
+          max='36'
+          step='2'
+          value={fontSize}
+          onChange={(e) => handleFontSizeChange(parseInt(e.target.value))}
+          style={{ marginLeft: "10px" }}
+        />
+        <input
+          type='range'
+          min='-180'
+          max='180'
+          step='1'
+          value={rotation}
+          onChange={(e) => handleRotationChange(parseInt(e.target.value))}
+          style={{ marginLeft: "10px" }}
+        />
+      </div>
+    </div>
   );
 }
 
