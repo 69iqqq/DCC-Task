36dd276a11edba933b051c730a02b2e8cf9446e3
69iqqq <abirdasgupta09@gmail.com>
2024-07-07 22:47:38 +0530

    text rotation by pinching

--- src/components/ForEditing.jsx
+++ src/components/ForEditing.jsx
@@ -149,6 +149,7 @@ function ForEditing({ style }) {
                   resize: "none",
                   transform: `rotate(${rotation}deg)`,
                   transformOrigin: "center",
+                  position: "absolute",
                 }}
               />
             </div>
@@ -162,6 +163,7 @@ function ForEditing({ style }) {
                 transform: `rotate(${rotation}deg)`,
                 transformOrigin: "center",
                 display: "inline-block",
+                position: "absolute",
               }}
               onClick={enterEditMode}
               onTouchStart={handleTouchStart}
