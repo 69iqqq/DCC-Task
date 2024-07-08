8c1f6e74b242e9fd0d325cea606ea173ac5f3f99
69iqqq <abirdasgupta09@gmail.com>
2024-07-08 23:54:42 +0530

    added size toggle option

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -512,6 +512,370 @@
 // }
 
 // export default Page;
+// import React, { useState, createRef, useEffect } from "react";
+// import { useSearchParams } from "react-router-dom";
+// import ForEditing from "../components/ForEditing";
+// import { exportComponentAsJPEG } from "react-component-export-image";
+// import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+// import MenuItem from "@mui/material/MenuItem";
+// import Select from "@mui/material/Select";
+// import FormControl from "@mui/material/FormControl";
+
+// function Page() {
+//   const downloadRef = createRef();
+//   const [params] = useSearchParams();
+//   const [text, setText] = useState(0);
+//   const [isMobile, setIsMobile] = useState(false);
+//   const [selectedSize, setSelectedSize] = useState("50%");
+
+//   const sizeOptions = [
+//     { id: "50%", label: "50%" },
+//     { id: "60%", label: "60%" },
+//     { id: "70%", label: "70%" },
+//     { id: "80%", label: "80%" },
+//     { id: "90%", label: "90%" },
+//     { id: "100%", label: "100%" },
+//     { id: "110%", label: "110%" },
+//     { id: "120%", label: "120%" },
+//   ];
+
+//   const setSize = (event) => {
+//     setSelectedSize(event.target.value);
+//   };
+
+//   const addText = () => {
+//     setText(text + 1);
+//   };
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setIsMobile(window.innerWidth <= 768);
+//     };
+
+//     handleResize();
+//     window.addEventListener("resize", handleResize);
+//     return () => {
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, []);
+
+//   return (
+//     <div
+//       style={{
+//         margin: "auto",
+//         textAlign: "center",
+//         backgroundColor: "black",
+//         width: "100%",
+//         height: "100vh",
+//         display: "flex",
+//         flexDirection: "column",
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <div
+//         style={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//           marginBottom: "10px",
+//         }}
+//       >
+//         <div
+//           ref={downloadRef}
+//           style={{
+//             maxWidth: selectedSize,
+//             width: "97.3%",
+//             position: "relative",
+//           }}
+//         >
+//           <img
+//             src={params.get("url")}
+//             alt='Meme Template'
+//             style={{
+//               maxWidth: "100%",
+//               width: "100%",
+//               boxShadow: "0px 3px 3px 3px #38363a1d",
+//               marginBottom: "10px",
+//             }}
+//           />
+
+//           <div
+//             style={{
+//               position: "absolute",
+//               top: 0,
+//               left: 0,
+//               width: "100%",
+//               height: "100%",
+//             }}
+//           >
+//             {Array(text)
+//               .fill(0)
+//               .map((_, index) => (
+//                 <ForEditing key={index} style={{ position: "absolute" }} />
+//               ))}
+//           </div>
+//         </div>
+
+//         <div
+//           style={{
+//             maxWidth: "300px",
+//             marginLeft: "20px",
+//             display: "flex",
+//             flexDirection: "column",
+//             alignItems: "center",
+//             gap: "20px",
+//           }}
+//         >
+//           <span
+//             style={{
+//               color: "#ffffff",
+//               fontWeight: "bold",
+//               cursor: "pointer",
+//             }}
+//             onClick={addText}
+//           >
+//             <span
+//               style={{
+//                 fontSize: "18px",
+//                 fontFamily: "Baskervville SC",
+//               }}
+//             >
+//               T
+//             </span>
+//           </span>
+//           <span
+//             style={{
+//               color: "#ffffff",
+//               fontWeight: "bold",
+//               cursor: "pointer",
+//             }}
+//             onClick={() => exportComponentAsJPEG(downloadRef)}
+//           >
+//             <DownloadOutlinedIcon />
+//           </span>
+//           <FormControl style={{ marginTop: "20px", width: "20px" }}>
+//             <Select
+//               value={selectedSize}
+//               onChange={setSize}
+//               displayEmpty
+//               style={{
+//                 backgroundColor: "transparent",
+//                 color: "#fff",
+//                 borderRadius: "3px",
+//                 border: "none",
+//                 fontSize: "12px",
+//                 height: "28px",
+//                 width: "60px",
+//               }}
+//               MenuProps={{
+//                 anchorOrigin: {
+//                   vertical: "bottom",
+//                   horizontal: "left",
+//                 },
+//                 transformOrigin: {
+//                   vertical: "top",
+//                   horizontal: "left",
+//                 },
+//                 getContentAnchorEl: null,
+//               }}
+//             >
+//               {sizeOptions.map((option) => (
+//                 <MenuItem key={option.id} value={option.id}>
+//                   {option.label}
+//                 </MenuItem>
+//               ))}
+//             </Select>
+//           </FormControl>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Page;
+// import React, { useState, createRef, useEffect } from "react";
+// import { useSearchParams } from "react-router-dom";
+// import ForEditing from "../components/ForEditing";
+// import { exportComponentAsJPEG } from "react-component-export-image";
+// import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+// import MenuItem from "@mui/material/MenuItem";
+// import Select from "@mui/material/Select";
+// import FormControl from "@mui/material/FormControl";
+
+// function Page() {
+//   const downloadRef = createRef();
+//   const [params] = useSearchParams();
+//   const [text, setText] = useState(0);
+//   const [isMobile, setIsMobile] = useState(false);
+//   const [selectedSize, setSelectedSize] = useState("50%");
+
+//   const sizeOptions = [
+//     { id: "50%", label: "50%" },
+//     { id: "60%", label: "60%" },
+//     { id: "70%", label: "70%" },
+//     { id: "80%", label: "80%" },
+//     { id: "90%", label: "90%" },
+//     { id: "100%", label: "100%" },
+//     { id: "110%", label: "110%" },
+//     { id: "120%", label: "120%" },
+//   ];
+
+//   const setSize = (event) => {
+//     setSelectedSize(event.target.value);
+//   };
+
+//   const addText = () => {
+//     setText(text + 1);
+//   };
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setIsMobile(window.innerWidth <= 768);
+//     };
+
+//     handleResize();
+//     window.addEventListener("resize", handleResize);
+//     return () => {
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, []);
+
+//   return (
+//     <div
+//       style={{
+//         margin: "auto",
+//         textAlign: "center",
+//         backgroundColor: "#5a5e77",
+//         width: "100%",
+//         height: "100vh",
+//         display: "flex",
+//         flexDirection: "column",
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <div
+//         style={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//           marginBottom: "10px",
+//         }}
+//       >
+//         <div
+//           ref={downloadRef}
+//           style={{
+//             maxWidth: selectedSize,
+//             width: "97.3%",
+//             position: "relative",
+//           }}
+//         >
+//           <img
+//             src={params.get("url")}
+//             alt='Meme Template'
+//             style={{
+//               maxWidth: "100%",
+//               width: "100%",
+//               boxShadow: "0px 3px 3px 3px #38363a1d",
+//               marginBottom: "10px",
+//             }}
+//           />
+
+//           <div
+//             style={{
+//               position: "absolute",
+//               top: 0,
+//               left: 0,
+//               width: "100%",
+//               height: "100%",
+//             }}
+//           >
+//             {Array(text)
+//               .fill(0)
+//               .map((_, index) => (
+//                 <ForEditing key={index} style={{ position: "absolute" }} />
+//               ))}
+//           </div>
+//         </div>
+
+//         <div
+//           style={{
+//             maxWidth: "300px",
+//             marginLeft: "20px",
+//             display: "flex",
+//             flexDirection: "column",
+//             alignItems: "center",
+//             gap: "20px",
+//           }}
+//         >
+//           <span
+//             style={{
+//               color: "#ffffff",
+//               fontWeight: "bold",
+//               cursor: "pointer",
+//             }}
+//             onClick={addText}
+//           >
+//             <span
+//               style={{
+//                 fontSize: "18px",
+//                 fontFamily: "Baskervville SC",
+//               }}
+//             >
+//               T
+//             </span>
+//           </span>
+//           <span
+//             style={{
+//               color: "#ffffff",
+//               fontWeight: "bold",
+//               cursor: "pointer",
+//             }}
+//             onClick={() => exportComponentAsJPEG(downloadRef)}
+//           >
+//             <DownloadOutlinedIcon />
+//           </span>
+//           <FormControl style={{ marginTop: "20px", width: "20px" }}>
+//             <Select
+//               value={selectedSize}
+//               onChange={setSize}
+//               displayEmpty
+//               style={{
+//                 backgroundColor: "transparent",
+//                 color: "#fff",
+//                 borderRadius: "3px",
+//                 border: "none",
+//                 fontSize: "12px",
+//                 height: "28px",
+//                 width: "60px",
+//               }}
+//               MenuProps={{
+//                 anchorOrigin: {
+//                   vertical: "bottom",
+//                   horizontal: "left",
+//                 },
+//                 transformOrigin: {
+//                   vertical: "top",
+//                   horizontal: "left",
+//                 },
+//                 getContentAnchorEl: null,
+//               }}
+//             >
+//               {sizeOptions.map((option) => (
+//                 <MenuItem key={option.id} value={option.id}>
+//                   {option.label}
+//                 </MenuItem>
+//               ))}
+//             </Select>
+//           </FormControl>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Page;
 import React, { useState, createRef, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import ForEditing from "../components/ForEditing";
@@ -564,7 +928,7 @@ function Page() {
       style={{
         margin: "auto",
         textAlign: "center",
-        backgroundColor: "black",
+        backgroundColor: "#20202c",
         width: "100%",
         height: "100vh",
         display: "flex",
@@ -666,7 +1030,7 @@ function Page() {
                 border: "none",
                 fontSize: "12px",
                 height: "28px",
-                width: "60px",
+                width: "80px",
               }}
               MenuProps={{
                 anchorOrigin: {
