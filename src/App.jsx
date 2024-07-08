d17b7586c01eb844f606acd0574f30fa6efd34c7
69iqqq <abirdasgupta09@gmail.com>
2024-07-08 23:07:21 +0530

    added size toggle option

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,63 @@
-import "./App.css"
-import { Routes, Route } from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
-import Home from "./pages/Home"
-import Page from "./pages/Page"
-import Footer from "./components/Footer"
-// import Header from "./components/Header"
+// import "./App.css"
+// import { Routes, Route } from "react-router-dom"
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import Home from "./pages/Home"
+// import Page from "./pages/Page"
+// import Footer from "./components/Footer"
+// // import Header from "./components/Header"
+// function App() {
+//   return (
+//     <div>
+//       <div className="App" >
+//         {/* <Header /> */}
+//         <Routes>
+//           {<Route path="/" element={<Home/>}/>}
+//           <Route path="/edit" element={<Page/>}/>
+//         </Routes>
+//         <Footer />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Page from "./pages/Page";
+import Footer from "./components/Footer";
+import Footer2 from "./components/Footer2"; // Import Footer2 component
+import "./App.css";
+
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as needed
+    };
+
+    handleResize(); // Check the screen size on initial render
+
+    window.addEventListener("resize", handleResize); // Add resize event listener
+    return () => window.removeEventListener("resize", handleResize); // Cleanup on unmount
+  }, []);
+
   return (
     <div>
-      <div className="App" >
+      <div className='App'>
         {/* <Header /> */}
         <Routes>
-          {<Route path="/" element={<Home/>}/>}
-          <Route path="/edit" element={<Page/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/edit' element={<Page />} />
         </Routes>
-        <Footer />
+        {isMobile ? <Footer2 /> : <Footer />}{" "}
+        {/* Conditionally render Footer or Footer2 */}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -305,6 +305,213 @@
 // }
 
 // export default Page;
+// import React, { useState, createRef, useEffect } from "react";
+// import { useSearchParams } from "react-router-dom";
+// import ForEditing from "../components/ForEditing";
+// import { exportComponentAsJPEG } from "react-component-export-image";
+// import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+// import MenuItem from "@mui/material/MenuItem";
+// import Select from "@mui/material/Select";
+// import FormControl from "@mui/material/FormControl";
+
+// function Page() {
+//   const downloadRef = createRef();
+//   const [params] = useSearchParams();
+//   const [text, setText] = useState(0);
+//   const [isMobile, setIsMobile] = useState(false);
+//   const [selectedSize, setSelectedSize] = useState("50%"); 
+
+//   const sizeOptions = [
+//     { id: "50%", label: "50%" },
+//     { id: "60%", label: "60%" },
+//     { id: "70%", label: "70%" },
+//     { id: "80%", label: "80%" },
+//     { id: "90%", label: "90%" },
+//     { id: "100%", label: "100%" },
+//     { id: "110%", label: "110%" },
+//     { id: "120%", label: "120%" },
+//   ];
+
+//   const setSize = (event) => {
+//     setSelectedSize(event.target.value);
+//   };
+
+//   const addText = () => {
+//     setText(text + 1);
+//   };
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setIsMobile(window.innerWidth <= 768);
+//     };
+
+//     handleResize();
+//     window.addEventListener("resize", handleResize);
+//     return () => {
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, []);
+
+//   return (
+//     <div
+//       style={{
+//         margin: "auto",
+//         textAlign: "center",
+//       }}
+//     >
+//       <h1
+//         className='text-center'
+//         style={{
+//           color: "#f8f8f2",
+//           fontFamily: "Outfit",
+//           fontWeight: "bold",
+//           marginTop:"20px"
+//         }}
+//       >
+//         Humour Check
+//       </h1>
+
+//       <div
+//         style={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//           marginTop: "20px",
+//           marginBottom: "10px",
+//         }}
+//       >
+//         <div
+//           ref={downloadRef}
+//           style={{
+//             maxWidth: selectedSize,
+//             width: "97.3%",
+//             position: "relative",
+//           }}
+//         >
+//           <img
+//             src={params.get("url")}
+//             alt='Meme Template'
+//             style={{
+//               maxWidth: "100%",
+//               width: "100%",
+//               boxShadow: "0px 3px 3px 3px #38363a1d",
+//               marginBottom: "10px",
+//             }}
+//           />
+
+//           <div
+//             style={{
+//               position: "absolute",
+//               top: 0,
+//               left: 0,
+//               width: "100%",
+//               height: "100%",
+//             }}
+//           >
+//             {Array(text)
+//               .fill(0)
+//               .map((_, index) => (
+//                 <ForEditing key={index} style={{ position: "absolute" }} />
+//               ))}
+//           </div>
+//         </div>
+
+//         <div
+//           style={{
+//             maxWidth: "300px",
+//             marginLeft: "20px", 
+//             display: "flex",
+//             flexDirection: "column",
+//             alignItems: "center", 
+//             gap: "20px", 
+//           }}
+//         >
+//           <span
+//             style={{
+//               color: "#ffffff",
+//               fontWeight: "bold",
+//               cursor: "pointer",
+//             }}
+//             onClick={addText}
+//           >
+//             <span
+//               style={{
+//                 fontSize: "18px",
+//                 fontFamily: "Baskervville SC",
+//               }}
+//             >
+//               T
+//             </span>
+//           </span>
+//           <span
+//             style={{
+//               color: "#ffffff",
+//               fontWeight: "bold",
+//               cursor: "pointer",
+//             }}
+//             onClick={() => exportComponentAsJPEG(downloadRef)}
+//           >
+//             <DownloadOutlinedIcon />
+//           </span>
+//           <FormControl style={{ marginTop: "20px", width: "20px" }}>
+//             {" "}
+//             <Select
+//               value={selectedSize}
+//               onChange={setSize}
+//               displayEmpty
+//               style={{
+//                 backgroundColor: "transparent",
+//                 color: "#fff",
+//                 borderRadius: "3px",
+//                 // border: "1px solid #fff",
+//                 border: "none",
+                
+//                 fontSize: "12px",
+//                 height: "28px",
+//                 width: "60px", 
+//               }}
+//               MenuProps={{
+//                 anchorOrigin: {
+//                   vertical: "bottom",
+//                   horizontal: "left",
+//                 },
+//                 transformOrigin: {
+//                   vertical: "top",
+//                   horizontal: "left",
+//                 },
+//                 getContentAnchorEl: null,
+//               }}
+//             >
+//               {sizeOptions.map((option) => (
+//                 <MenuItem key={option.id} value={option.id}>
+//                   {option.label}
+//                 </MenuItem>
+//               ))}
+//             </Select>
+//           </FormControl>
+//         </div>
+//       </div>
+
+//       <p style={{ color: "#f8f8f2" }}>
+//         For mobile 2-finger tap on the text to edit. Pinch to resize and rotate
+//         text
+//         <br />
+//         <span
+//           style={{
+//             padding: "2px 5px",
+//             borderRadius: "3px",
+//             fontFamily: "Poppins",
+//             color: "#ff79c6",
+//           }}
+//         >
+//           Make sure that nothing overlays the image.
+//         </span>
+//       </p>
+//     </div>
+//   );
+// }
+
+// export default Page;
 import React, { useState, createRef, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import ForEditing from "../components/ForEditing";
@@ -319,7 +526,7 @@ function Page() {
   const [params] = useSearchParams();
   const [text, setText] = useState(0);
   const [isMobile, setIsMobile] = useState(false);
-  const [selectedSize, setSelectedSize] = useState("50%"); 
+  const [selectedSize, setSelectedSize] = useState("50%");
 
   const sizeOptions = [
     { id: "50%", label: "50%" },
@@ -357,26 +564,20 @@ function Page() {
       style={{
         margin: "auto",
         textAlign: "center",
+        backgroundColor: "black",
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <h1
-        className='text-center'
-        style={{
-          color: "#f8f8f2",
-          fontFamily: "Outfit",
-          fontWeight: "bold",
-          marginTop:"20px"
-        }}
-      >
-        Humour Check
-      </h1>
-
       <div
         style={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          marginTop: "20px",
           marginBottom: "10px",
         }}
       >
@@ -419,11 +620,11 @@ function Page() {
         <div
           style={{
             maxWidth: "300px",
-            marginLeft: "20px", 
+            marginLeft: "20px",
             display: "flex",
             flexDirection: "column",
-            alignItems: "center", 
-            gap: "20px", 
+            alignItems: "center",
+            gap: "20px",
           }}
         >
           <span
@@ -454,7 +655,6 @@ function Page() {
             <DownloadOutlinedIcon />
           </span>
           <FormControl style={{ marginTop: "20px", width: "20px" }}>
-            {" "}
             <Select
               value={selectedSize}
               onChange={setSize}
@@ -463,12 +663,10 @@ function Page() {
                 backgroundColor: "transparent",
                 color: "#fff",
                 borderRadius: "3px",
-                // border: "1px solid #fff",
                 border: "none",
-                
                 fontSize: "12px",
                 height: "28px",
-                width: "60px", 
+                width: "60px",
               }}
               MenuProps={{
                 anchorOrigin: {
@@ -491,22 +689,6 @@ function Page() {
           </FormControl>
         </div>
       </div>
-
-      <p style={{ color: "#f8f8f2" }}>
-        For mobile 2-finger tap on the text to edit. Pinch to resize and rotate
-        text
-        <br />
-        <span
-          style={{
-            padding: "2px 5px",
-            borderRadius: "3px",
-            fontFamily: "Poppins",
-            color: "#ff79c6",
-          }}
-        >
-          Make sure that nothing overlays the image.
-        </span>
-      </p>
     </div>
   );
 }
